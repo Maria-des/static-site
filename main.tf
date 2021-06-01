7b569e6d67938c138c5f1f90de767dfbbfafc409
root <root@jenkins.local>
2021-06-01 20:18:07 +0200

    add index

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "backet1" {
   bucket = "myterabucket"
   acl    = "public-read"
-
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
   tags = {
     Name        = "Maria bucket"
   }
+}
+resource "aws_s3_bucket_object" "index" {
+  source = "index.html"
+  bucket = "myterabucket"
+  acl    = "public-read"
+  key = "index.html"
+  content_type = "text/html"
 }
